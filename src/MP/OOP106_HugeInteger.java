e23b4ece0301d425c2c6a15760425983fd821b63
brant <brant@172.16.16.192>
2018-09-24 07:23:26 +0800

    finished parse method

--- src/MP/OOP106_HugeInteger.java
+++ src/MP/OOP106_HugeInteger.java
@@ -2,17 +2,32 @@ package MP;
 
 public class OOP106_HugeInteger {
 	public static int[] intArray = new int[40];
+	public static int integerLength;
 	
 	public OOP106_HugeInteger(){
 		
 	}
 	
-	public static int parse(String numbersHiding) {
+	public static void parse(String numbersHiding) {
+		String integerStr = "";
+		
+		//Parse digits in string
 		for (int numlength = 0; numlength < numbersHiding.length(); numlength++) {
-			
-			
+			Character n = numbersHiding.charAt(numlength);
+			if (Character.isDigit(n)) {
+				integerStr += n;
+			}
 		}
-		return 0;
+		
+		//Check if string passes integer size
+		integerLength = (integerStr.length() > intArray.length) ? intArray.length : integerStr.length();
+		
+		//Adding to array
+		int n = 0;
+		
+		for (int i = 40 - integerLength; i < 40; i++) {
+			intArray[i] = integerStr.charAt(n++) - 48;
+		}
 	}
 	
 	public static void add() {
