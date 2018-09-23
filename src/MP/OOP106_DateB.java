a1e59b0628870e7c9f1a8bc8c086669ebaa5a053
brant <brant@172.16.16.192>
2018-09-24 07:04:39 +0800

    Started DateB

--- src/MP/OOP106_DateB.java
+++ src/MP/OOP106_DateB.java
@@ -2,7 +2,7 @@ package MP;
 
 public class OOP106_DateB {
 	public static String format;
-	public static int day, year, monthInt;
+	public static int day, year, monthInt, daysYearFormat;
 	public static String monthStr;
 	public static String[] monthArray = {
 		"", "January", "February", "March", "April", "May", "June",
@@ -12,12 +12,36 @@ public class OOP106_DateB {
 		0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
 	};
 	
+	public static int[] date = new int[3];
+	
+	
+	
 	public OOP106_DateB(){
-		
+		date[0] = 0;
+        date[1] = 1;
+        date[2] = 1;
+		date[3] = 1;
 	}
 	
-	public static String format(int monthInt, int day, int year) {
-		format = String.format("%02d/%02d/%04d", monthInt, day, year);
+	public OOP106_DateB(int monthInt, int day, int year){
+        setYear(year);
+        setMonthInt(monthInt);
+        setDay(day);
+    }
+	
+	public OOP106_DateB(String monthStr, int day, int year){
+        setYear(year);
+        setMonthStr(monthStr);
+        setDay(day);
+    }
+	
+	public OOP106_DateB(int days, int year){
+        setYear(year);
+        setDaysYearFormat(days);
+    }
+	
+	public static String format1() {
+		format = String.format("%02d/%02d/%04d", date[1], date[2], date[3]);
 		return format;
 	}
 	
@@ -73,6 +97,10 @@ public class OOP106_DateB {
 
 	public static void setMonthInt(int monthInt) {
 		OOP106_DateB.monthInt = monthInt;
+	}
+
+	public static void setDaysYearFormat(int daysYearFormat) {
+		OOP106_DateB.daysYearFormat = daysYearFormat;
 	}
 	
 	
--- src/MP/OOP106_DateBDemo.java
+++ src/MP/OOP106_DateBDemo.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class OOP106_DateBDemo {
 	
 	public static void main(String[] args) {
-		
+		OOP106_DateB d1 = new OOP106_DateB(11, 13, 2016);
+        OOP106_DateB d2 = new OOP106_DateB("February", 29, 1988);
+        OOP106_DateB d3 = new OOP106_DateB(317, 2015);
 	}
 	
 	
