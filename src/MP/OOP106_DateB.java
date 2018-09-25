bbb861704eaaff97f46930b4bc914b9d516db499
brant <brant@172.16.16.192>
2018-09-25 20:39:07 +0800

    fixing overloading

--- src/MP/OOP106_DateB.java
+++ src/MP/OOP106_DateB.java
@@ -12,9 +12,7 @@ public class OOP106_DateB {
 		0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
 	};
 	
-	public static int[] date = new int[3];
-	
-	
+	public static int[] date = new int[4];
 	
 	public OOP106_DateB(){
 		date[0] = 0;
@@ -24,49 +22,48 @@ public class OOP106_DateB {
 	}
 	
 	public OOP106_DateB(int monthInt, int day, int year){
-        setYear(year);
-        setMonthInt(monthInt);
+//        setYear(year);
+//        setMonthInt(monthInt);
         setDay(day);
     }
 	
 	public OOP106_DateB(String monthStr, int day, int year){
-        setYear(year);
+//        setYear(year);
         setMonthStr(monthStr);
         setDay(day);
     }
 	
-	public OOP106_DateB(int days, int year){
-        setYear(year);
-        setDaysYearFormat(days);
-    }
+//	public OOP106_DateB(int days, int year){
+//        setYear(year);
+//        setDaysYearFormat(days);
+//    }
 	
-	public static String format1() {
-		format = String.format("%02d/%02d/%04d", date[1], date[2], date[3]);
-		return format;
+	public String format1() {
+		return String.format("%02d/%02d/%04d", date[1], date[2], date[3]);
 	}
 	
-	public static String format(String monthStr, int day, int year) {
-		for (int i = 1; i < monthArray.length; i++) {
-			if (monthStr.equalsIgnoreCase(monthArray[i])) {
-				monthStr = monthArray[i];
-				break;
-			}
-		}
-		format = String.format(monthStr +" %02d, %04d", day, year);
-		return format;
-	}
+//	public static String format(String monthStr, int day, int year) {
+//		for (int i = 1; i < monthArray.length; i++) {
+//			if (monthStr.equalsIgnoreCase(monthArray[i])) {
+//				monthStr = monthArray[i];
+//				break;
+//			}
+//		}
+//		format = String.format(monthStr +" %02d, %04d", day, year);
+//		return format;
+//	}
 	
-	public static int format(int daysInYear, int year) {
-		return 0;
-	}
+//	public static int format(int daysInYear, int year) {
+//		return 0;
+//	}
 	
-	public static boolean isLeapYear(int year) {
-		if ((year % 4 == 0) && (year % 400 == 0) || (year % 100 != 0)) {
-			return true;
-		} else {
-			return false;
-		}
-		
+//	public static boolean isLeapYear(int year) {
+//		if ((year % 4 == 0) && (year % 400 == 0) || (year % 100 != 0)) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+//		
 //		Leap Year Conditions:
 //		1. Divisible by 4
 //		2. Not divisible by 100 OR divisible by 400
@@ -81,14 +78,15 @@ public class OOP106_DateB {
 //		} else {
 //			return false;
 //		}
-	}
+//	}
 
+	//Setters
 	public static void setDay(int day) {
-		OOP106_DateB.day = day;
+		date[2] = day;
 	}
 
 	public static void setYear(int year) {
-		OOP106_DateB.year = year;
+		date[3] = year;
 	}
 
 	public static void setMonthStr(String monthStr) {
@@ -96,11 +94,23 @@ public class OOP106_DateB {
 	}
 
 	public static void setMonthInt(int monthInt) {
-		OOP106_DateB.monthInt = monthInt;
+		date[1] = monthInt;
 	}
 
 	public static void setDaysYearFormat(int daysYearFormat) {
 		OOP106_DateB.daysYearFormat = daysYearFormat;
+	}
+
+	public static int getYear() {
+		return year;
+	}
+
+	public static int date1() {
+		return date[2];
+	}
+	
+	public static void returnDate(){
+		System.out.println(date[1]+" "+ date[2]+" "+ date[3]);
 	}
 	
 	
