30375a5009d84e27fa958433233cf5299b22242a
brant <brant@172.16.16.192>
2018-09-25 22:49:02 +0800

    Formatting

--- src/MP/OOP106_DateB.java
+++ src/MP/OOP106_DateB.java
@@ -4,6 +4,7 @@ public class OOP106_DateB {
 	public String format;
 	public int day, year, monthInt, daysYearFormat;
 	public String monthStr;
+	public int[] date = new int[4];
 	public String[] monthArray = {
 		"", "January", "February", "March", "April", "May", "June",
 		"July", "August", "September", "October", "November", "December"
@@ -12,7 +13,6 @@ public class OOP106_DateB {
 		0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
 	};
 	
-	public int[] date = new int[4];
 	
 	public OOP106_DateB(){
 		date[0] = 0;
@@ -20,7 +20,6 @@ public class OOP106_DateB {
         date[2] = 1;
 		date[3] = 1;
 	}
-	
 	public OOP106_DateB(int monthInt, int day, int year){
         setYear(year);
         setMonthInt(monthInt);
@@ -46,38 +45,23 @@ public class OOP106_DateB {
 		return String.format("%03d %04d", getDaysYearFormat(), date[3]);
 	}
 	
-	public String format(String monthStr, int day, int year) {
-		for (int i = 1; i < monthArray.length; i++) {
-			if (monthStr.equalsIgnoreCase(monthArray[i])) {
-				monthStr = monthArray[i];
-				break;
-			}
-		}
-		format = String.format(monthStr +" %02d, %04d", day, year);
-		return format;
-	}
-	
-	public int format(int daysInYear, int year) {
-		return 0;
-	}
-	
 	public static boolean isLeapYear(int year) {
 		return ((year % 4 == 0) && (year % 400 == 0) || (year % 100 != 0));
+	/*	Leap Year Conditions:
+		1. Divisible by 4
+		2. Not divisible by 100 OR divisible by 400
 		
-//		Leap Year Conditions:
-//		1. Divisible by 4
-//		2. Not divisible by 100 OR divisible by 400
-//		
-//		Ex. 
-//		if (year % 4 != 0) {
-//			return false;
-//		} else if (year % 400 == 0) {
-//			return true;
-//		} else if (year % 100 == 0) {
-//			return false;
-//		} else {
-//			return false;
-//		}
+		Ex. 
+		if (year % 4 != 0) {
+			return false;
+		} else if (year % 400 == 0) {
+			return true;
+		} else if (year % 100 == 0) {
+			return false;
+		} else {
+			return false;
+		}
+	*/
 	}
 
 	//Setters
@@ -93,30 +77,27 @@ public class OOP106_DateB {
 				date[1] = month;
 			}
 		}
-		
 	}
 	public void setMonthInt(int monthInt) {
 		date[1] = monthInt;
 	}
 	public void setDaysYearFormat(int daysYearFormat) {
-		
 		daysInMonths[2] = (isLeapYear(year) ? 29 : 28);
 		int month = 1;
 		while(daysYearFormat > daysInMonths[month]){
             daysYearFormat -= daysInMonths[month];
             month++;
         }
-		
 		setMonthInt(month);
 		setDay(daysYearFormat);
 	}
 
+	//Getters
 	public int getYear() {
 		return year;
 	}
 	public int getDaysYearFormat() {
 		int tempDay = 0;
-		
 		for (int month = 1; month < date[1]; month++) {
 			tempDay += daysInMonths[month];
 		}
