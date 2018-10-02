fd9dac9ca9e81b29d06d0753d163e2d12febf3f5
brant <brant@172.16.16.192>
2018-10-02 21:17:17 +0800

    Formatting

--- src/SavingsAccount/SavingsAccount.java
+++ src/SavingsAccount/SavingsAccount.java
@@ -1,16 +1,13 @@
 package SavingsAccount;
 
 import java.util.Scanner;
-
 public class SavingsAccount {
 	private static double annualInterestRate = 0.0;
 	private double savingsBalance = 0.0;
 	public static Scanner sc = new Scanner(System.in);
-	
 	public SavingsAccount(double savingsBalance){
 		setSavingsBalance(savingsBalance);
 	}
-	
 	public static void modifyInterestRate(double newInterestRate){
 		//Check for negative interest input
 		while(newInterestRate < 0.0){
@@ -19,18 +16,13 @@ public class SavingsAccount {
 		}
 		annualInterestRate = newInterestRate;
 	}
-	
 	public void calculateMonthlyInterest(){
 		savingsBalance += (savingsBalance + annualInterestRate) / 12;
 	}
-
 	@Override
 	public String toString() {
 		return String.format("%.2f", getSavingsBalance());
 	}
-	
-	
-
 	//Getters/Setters
 	public static double getAnnualInterestRate() {
 		return annualInterestRate;
