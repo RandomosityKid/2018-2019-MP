dfcffaca15417f9aee5d02df99fe071e6311709f
brant <brant@172.16.16.192>
2018-12-05 07:05:43 +0800

    Created Account (#5)

--- src/MP/OOP106_Account.java
+++ src/MP/OOP106_Account.java
@@ -2,19 +2,19 @@ package MP;
 
 import java.util.Date;
 
-public class OOP106_Account {
+class OOP106_AccountBase {
 	private int id = 0;
 	private double balance = 0.0;
 	private double annualInterestRate = 0.0;
 	private Date dateCreated;
 	
-	public OOP106_Account(){
-	
+	public OOP106_AccountBase(){
 	}
 	
-	public OOP106_Account(int id, double balance){
+	public OOP106_AccountBase(int id, double balance){
 		this.id = id;
 		this.balance = balance;
+		dateCreated = new Date();
 	}
 
 	public int getId() {
@@ -38,7 +38,7 @@ public class OOP106_Account {
 	}
 
 	public void setAnnualInterestRate(double annualInterestRate) {
-		this.annualInterestRate = annualInterestRate;
+		this.annualInterestRate = annualInterestRate/100;
 	}
 
 	public Date getDateCreated() {
@@ -60,12 +60,26 @@ public class OOP106_Account {
 			System.out.println("You don't have that much in your account.");
 		} else {
 			balance -= money;
-			System.out.println("Withdrew $" + money + "\n Current Balance: $" + balance + "\n");
+			System.out.println("\nWithdrew $" + money + "\nCurrent Balance: $" + balance + "\n");
 		}
 	}
 	
 	public void deposit(double money) {
 		balance += money;
-		System.out.println("Deposited $" + money + "\n Current Balance: $" + balance + "\n");
+		System.out.println("Deposited $" + money + "\nCurrent Balance: $" + balance + "\n");
+	}
+}
+
+public class OOP106_Account {
+	public static void main(String[] args) {
+		OOP106_AccountBase acct1 = new OOP106_AccountBase(1122, 20000);
+		acct1.setAnnualInterestRate(4.5);
+		
+		System.out.println("Account1 Balance: " + acct1.getBalance());
+		acct1.withdraw(2500);
+		acct1.deposit(3000);
+		System.out.println(	"\nAccount1 Balance: " + acct1.getBalance() + 
+							"\nAccount1 Monthly Interest:" + acct1.getMonthlyInterest() +
+							"\nAccount1 Date created:" + acct1.getDateCreated());
 	}
 }
